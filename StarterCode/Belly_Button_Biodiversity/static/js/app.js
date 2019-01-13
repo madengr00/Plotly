530b5ea84e8820c8326a7ba04d754f669b80329a
duelingthreads <duelingthreads@gmail.com>
2019-01-13 17:30:50 -0600

    added gauge plot

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -26,7 +26,67 @@ function buildMetadata(sample) {
     d3.select("#sample-metadata").append("li").text(`Location: ${location}`);
     d3.select("#sample-metadata").append("li").text(`Bbtype: ${bbtype}`);
     d3.select("#sample-metadata").append("li").text(`Wfreq: ${wfreq}`);
-  });  
+
+    //Create a Gauge Chart
+    // Enter a speed between 0 and 180
+    var level = wfreq;
+
+    // Trig to calc meter point
+    var degrees = 9 - level,
+      radius = .5;
+    var radians = degrees * Math.PI / 9;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+      pathX = String(x),
+      space = ' ',
+      pathY = String(y),
+      pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+    var data = [{ type: 'scatter',
+      x: [0], y:[0],
+      marker: {size: 28, color:'850000'},
+      showlegend: false,
+      name: 'speed',
+      text: level,
+      hoverinfo: 'text+name'},
+      { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+      rotation: 90,
+      text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3' , '1-2', '0-1',''],
+      textinfo: 'text',
+      textposition:'inside',
+      labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3' , '1-2', '0-1',''],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+      }];
+
+    var layout = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+      title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
+      // height: 1000,
+      // width: 1000,
+      xaxis: {zeroline:false, showticklabels:false,
+          showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+          showgrid: false, range: [-1, 1]}
+    };
+
+    Plotly.newPlot('gauge', data, layout);
+
+  });
+  
 }
   
     // HINT: You will need to use slice() to grab the top 10 sample_values,
@@ -38,6 +98,8 @@ function buildCharts(sample) {
   // Use `d3.json` to fetch the data
   d3.json(url).then(function(response){
     console.log(response);
+
+
     // Create a Pie Chart
     console.log(response.sample_values);
     console.log(response.otu_ids);
@@ -48,13 +110,14 @@ function buildCharts(sample) {
       sort: true
     }];
     var layout = {
-      title: "Top 10 Samples",
+      title: "<b> Top 10 Samples </b>",
     }
 
     console.log(trace1);
     //Plot the chart to a dive tag with id "pie"
     Plotly.newPlot("pie",trace1,layout);
     
+
     //Create a Bubble Chart
     var trace2 = [{
       y: response.sample_values,
@@ -74,9 +137,6 @@ function buildCharts(sample) {
     }
     //Plot the chart to a dive tag with id "bubble"
     Plotly.newPlot("bubble",trace2,layout);
-
-
-
   });
 }
 
